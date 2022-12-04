716df80780024137f749ab56eb9fa1feb54c481c
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-12-04 14:17:41 +0000

    adds questions

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// Questions, choices and answers in  an array with inner object elements
+var questions = [
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        choices: ["1. msg('Hello World')", "2. alert('Hello World')", "3. prompt('Hello World')",  "4. alertBox('Hello World')"],
+        answer: "2. alert('Hello World')"
+    },
+    {
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["1. if i = 5 then", "2. if i = 5", "3. if (i == 5)", "4. if i == 5 then"],
+        answer: "3. if (i == 5)"
+    },
+    {
+        question: "How does a FOR loop start?",
+        choices: ["1. for (i = 0; i<= 5; i++)", "2. for (i <= 5; i++)", "3. for i = 1 to 5", "4. for (i = 0; i <= 5)"],
+        answer: "1. for (i = 0; i<= 5; i++)"
+    },
+    {
+        question: "What is the correct way to write a JavaScript array?",
+        choices: ["1. var colors = (1:'red', 2:'green', 3:'blue')", "2. var colors = ['red', 'green', 'blue']", "3. var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", "4. var colors = 'red', 'green', 'blue'"],
+        answer: "2. var colors = ['red', 'green', 'blue']"
+    },
+    {
+        question: "Which event occurs when the user clicks on an HTML element?",
+        choices: ["1. onclick", "2. onchange", "3. onmouseover", "4. onmouseclick"],
+        answer: "1. onclick"
+    },
+];
