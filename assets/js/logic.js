af050ef2a4c3607c8f29c23fb0f6fd4d94771c92
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-12-04 18:01:26 +0000

    adds function to initiate quiz

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,19 +1,43 @@
 // Reference variables for the quiz DOM elements
-var questions = document.getElementById("questions");
-var choices = document.getElementById("choices");
-var startQuizButton = document.getElementById("start");
-var submitButton = document.getElementById("submit");
-var timerEl = document.getElementById("time");
-var initials = document.getElementById("initials");
-var feedback = document.getElementById("feedback");
+var questionsWrap = document.querySelector('#questions');
+var questionTitle = document.querySelector('#question-title');
+var questionChoices = document.querySelector('#choices');
+var startQuizButton = document.querySelector('#start');
+var submitButton = document.querySelector('#submit');
+var timerEl = document.querySelector('#time');
+var initials = document.querySelector('#initials');
+var feedback = document.querySelector('#feedback');
 // Other needed variables for the quiz
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
-var timerId;
-
 
 function timer() {
     // decrease time every second
     time--;
     timerEl.textContent = time;
 }
+
+function startQuiz() {
+    // add hide class to start screen
+    var startScreen = document.querySelector('#start-screen');
+    startScreen.classList.add('hide');
+
+    // remove hide class for questions section
+    questionsWrap.classList.remove('hide');
+
+    var currentQuestion = questions[currentQuestionIndex];
+    var choices = currentQuestion.choices;
+
+    questionTitle.innerText = currentQuestion.question;
+
+    for (var i = 0; i < choices.length; i++) {
+        var choice = choices[i];
+        var isCorrect = currentQuestion.answer === choice;
+
+        questionChoices.insertAdjacentHTML('beforeend', `<button data-correct=${isCorrect}>${choice}</button`);
+    }
+}
+
+startQuizButton.addEventListener('click', startQuiz);
+
+
