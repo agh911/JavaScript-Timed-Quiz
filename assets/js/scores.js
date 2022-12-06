36714561817263ff9942c0bdf2df7d99e6ae9615
Andreea Ghenea <andreeag@Andreeas-MBP.lan>
2022-12-06 17:41:43 +0000

    adds code to sort highscores from high to low

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,21 +3,25 @@ function displayHighscores() {
   // Grab scores from the localStorage
   var allHighscores = JSON.parse(localStorage.getItem("highscores"));
   if (allHighscores != null) {
+    // sort highscores: high -> low
+    allHighscores.sort(function (a, b) {
+      return parseInt(b.score) - parseInt(a.score);
+    });
     // for loop to go through scores and grab each one of them for display
-    for(var i = 0; i < allHighscores.length; i++){
+    for (var i = 0; i < allHighscores.length; i++) {
       // create <li> tag
       var scoreLi = document.createElement("li");
       scoreLi.textContent = allHighscores[i].initials + " - " + allHighscores[i].score;
       // and display it on the board
       document.querySelector("#highscores").appendChild(scoreLi);
-    } 
+    }
   } else {
     var highscoreBoard = document.querySelector("#highscores");
     highscoreBoard.textContent = "Highscores Board Cleared";
     setInterval(function () {
       highscoreBoard.classList.add('hide');
-  }, 1000);
-  }  
+    }, 1000);
+  }
 }
 
 // Create function to clear the highscores board
@@ -27,7 +31,7 @@ function clearBoard() {
 }
 // add event listener to the 'Clear' button and call function clearBoard
 var clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", function(){
+clearButton.addEventListener("click", function () {
   clearBoard();
 })
 
